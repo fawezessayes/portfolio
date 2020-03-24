4d6ede6c7c763794695f93487cb9a61ddecaa82d
Fawez Essayes <58982115+fawezessayes@users.noreply.github.com>
2020-03-24 11:46:21 +0100

    Updates

---
+++ precache-manifest.2870df8fd2340c2cc87425a605fa1df5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e18e767aaa9ce35f2d2aebf1b90cb26",
+    "url": "/fawezessayes.github.io/index.html"
+  },
+  {
+    "revision": "fe69bfa1a72d5ebb2952",
+    "url": "/fawezessayes.github.io/static/css/2.221380cf.chunk.css"
+  },
+  {
+    "revision": "fa8d5af256af76cf30d0",
+    "url": "/fawezessayes.github.io/static/css/main.23b90c2c.chunk.css"
+  },
+  {
+    "revision": "fe69bfa1a72d5ebb2952",
+    "url": "/fawezessayes.github.io/static/js/2.d8db953a.chunk.js"
+  },
+  {
+    "revision": "210e5fea5d657e0a340abfa54200e342",
+    "url": "/fawezessayes.github.io/static/js/2.d8db953a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8d5af256af76cf30d0",
+    "url": "/fawezessayes.github.io/static/js/main.db32ff1b.chunk.js"
+  },
+  {
+    "revision": "e082036b90e2ae1a32a9",
+    "url": "/fawezessayes.github.io/static/js/runtime-main.2e121e22.js"
+  }
+]);
